d1e4fc2bc313705ee99210dd56a61eb106185fb2
irohanrajput <itusharsingh27@gmail.com>
2024-02-22 00:39:02 +0530

    just trying some git branch stuff

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,10 +11,6 @@ def api_home(request, *args, **kwargs):
     data = {}
     
     if model_data:
-        # data['id']=model_data.id
-        # data['title']=model_data.title
-        # data['content']=model_data.content
-        # data['price']=model_data.price
         data = model_to_dict(model_data, fields=['id', 'title', 'price'] )
         
     return JsonResponse(data)
