77124dc12ec360677fcf0d69ecc38f67ab89ea76
irohanrajput <itusharsingh27@gmail.com>
2024-02-22 04:43:26 +0530

    done with 'GET' part.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,16 @@
-import json
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
 
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import PrductSerializer
 
+@api_view(['GET','POST'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'] )
+    if instance:
+        data = PrductSerializer(instance).data
         
-    return JsonResponse(data)
+    return Response(data)
