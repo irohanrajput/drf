b191ad2ab67455c7865823da70192bef3d84b2c8
irohanrajput <itusharsingh27@gmail.com>
2024-02-22 00:33:41 +0530

    trials

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,8 @@
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+from rest_framework.response import Response
 
 from products.models import Product
 
@@ -8,8 +11,10 @@ def api_home(request, *args, **kwargs):
     data = {}
     
     if model_data:
-        data['id']=model_data.id
-        data['title']=model_data.title
-        data['content']=model_data.content
-        data['price']=model_data.price
+        # data['id']=model_data.id
+        # data['title']=model_data.title
+        # data['content']=model_data.content
+        # data['price']=model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'] )
+        
     return JsonResponse(data)
