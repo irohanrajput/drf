b95a73df683964f012f87b46733ef89a9fe22512
irohanrajput <itusharsingh27@gmail.com>
2024-02-24 02:31:40 +0530

    whatever

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 
 from .models import Product
 
-class PrductSerializer(serializers.ModelSerializer):
+
+class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
@@ -11,10 +12,12 @@ class PrductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'my_discount'
+            'my_discount',
         ]
-        
+    
     def get_my_discount(self, obj):
-        print(obj.title)
-        #obj.username etc 
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
         return obj.get_discount()
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
 
-get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP 
+get_response = requests.post(endpoint, json={"title": "Hello world" }) # HTTP 
 
 print(get_response.json())
+ 
