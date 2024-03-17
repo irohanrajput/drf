6c89293afa735e8e88c4eca219d5cd713b5ad2f1
irohanrajput <itusharsingh27@gmail.com>
2024-03-17 20:10:33 +0530

    Refractored code, using hyperlinked relationship now, instead of primary key method, added some links to the snippet instance for easy navigation

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
-from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
@@ -26,3 +29,18 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })   
+    
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+    
+    def get(self, request, *args, **kwargs):
+        snippet_instance = self.get_object()
+        return Response(snippet_instance.highlighted)
