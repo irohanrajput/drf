e44195f98a7993c6e89e400acc3eb85138763180
irohanrajput <itusharsingh27@gmail.com>
2024-04-15 14:02:15 +0530

    system backup

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -35,5 +35,6 @@ urlpatterns = format_suffix_patterns([
     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
     path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail')
+    path('users/<int:pk>/', user_detail, name='user-detail'),
+    path('api-auth/', include('rest_framework.urls')),
 ])
